f7fd6b84685ddefda45a9951cd89e4579b3853cc
deeravathayyappa <ayyappanaikdeeravath@gmail.com>
2026-09-07 12:30:34 +0530

    Add files via upload

---
+++ postive.py
@@ -0,0 +1,14 @@
+num = int(input(" Enter a number: "))
+
+if num > 0:
+    print(" Given number is postitive number ")
+elif num < 0:
+    print(" Given number is negative number ")
+else:
+    print(" Given number is zero")
+
+
+'''output-
+Enter a number: -105Enter a number: -233
+  Given number is negative number
+  '''
