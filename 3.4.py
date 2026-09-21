2ac23e3f27d42bd3caf0d699dd529062cbd9616b
deeravathayyappa <ayyappanaikdeeravath@gmail.com>
2026-09-21 12:14:10 +0530

    Add files via upload

---
+++ 3.4.py
@@ -0,0 +1,21 @@
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a >= b:
+    if a >= c:
+        print("Largest number is:", a)
+    else:
+        print("Largest number is:", c)
+else:
+    if b >= c:
+        print("Largest number is:", b)
+    else:
+        print("Largest number is:", c)
+
+
+'''output: Enter first number: 3
+Enter second number: 10
+Enter third number: 20
+Largest number is: 20'''
