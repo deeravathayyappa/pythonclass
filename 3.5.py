b2807040fdb34f209a6607239111bef696cbe7f7
deeravathayyappa <ayyappanaikdeeravath@gmail.com>
2026-09-26 12:12:04 +0530

    Add files via upload

---
+++ 3.5.py
@@ -0,0 +1,20 @@
+marks = int(input("Enter student's marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Grade:", grade)
+
+'''output:
+Enter student's marks: 080
+Grade: B
+Enter student's marks: 16
+Grade: F'''
